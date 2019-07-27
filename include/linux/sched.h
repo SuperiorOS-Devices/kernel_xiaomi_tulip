b11d6d890bb5e3e6b9e5d48ca4580ea589c16c27
Julian Liu <wlootlxt123@gmail.com>
2019-07-28 02:32:43 +0800

    sched/tune: Expose sched_boost
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3581,21 +3581,8 @@ void cpufreq_remove_update_util_hook(int cpu);
 int do_stune_boost(char *st_name, int boost, int *slot);
 int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
-#else /* !CONFIG_DYNAMIC_STUNE_BOOST */
-static inline int do_stune_boost(char *st_name, int boost, int *slot)
-{
-	return 0;
-}
 
-static inline int do_stune_sched_boost(char *st_name, int *slot)
-{
-	return 0;
-}
-
-static inline int reset_stune_boost(char *st_name, int slot)
-{
-	return 0;
-}
+int get_sched_boost(char *st_name);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 #ifndef arch_scale_cpu_capacity
